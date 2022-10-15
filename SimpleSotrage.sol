ef54b512b7703a9c74b74def5a8b48b687eec3c3
Ilya Andreev <75562442+Bzelijah@users.noreply.github.com>
2022-10-15 17:01:02 +0300

    Create SimpleSotrage.sol

---
+++ SimpleSotrage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.8;
+
+contract SimpleStorage {
+
+    // default value is internal. it means this variable can only be called by this specific contract or derived contracts.  
+    uint256 favoriteNumber;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
